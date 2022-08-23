109bc35f879541bb5d5329521dd8c652713a3b39
Rabee <ralbhaisi96@gmail.com>
2022-08-23 22:18:59 +0300

    JS Create server finished writing app.js and index.js code relates #2

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+const path = require('path');
+const app = express();
+const router = require('./router');
+
+app.set('port', process.env.PORT || 4000);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(compression());
+app.use(helmet());
+
+app.get('/', (req, res)=>{
+  res.send(path(__dirname, '..', 'client', 'index.html'));
+});
+
+app.use('/api/v1/', router);
+
+app.use((req, res) => {
+  res.status(404).json({message: 'Not Found!!'});
+});
+
+app.use((req, res) => {
+  res.status(500)
+      .json({message: 'Website is under maintenance, please try again later'});
+});
+
+module.exports = app;
